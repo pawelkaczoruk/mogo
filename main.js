b4698fe091f6ebb81187348867f782c7f35213d6
fishu <pkaczoruk@wp.pl>
2019-10-25 11:28:12 +0200

    added scroll event handler and functions for statistics section count up

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // collapsible expanding and shrinking
 function expand(elem) {
   // get active element
-  const expanded = document.getElementsByClassName('active');
-  const activeBtn = expanded[0].previousElementSibling.getElementsByTagName('button');
-  const all = document.getElementsByClassName('collapsible-title');
+  const expanded = document.getElementsByClassName('active'),
+        activeBtn = expanded[0].previousElementSibling.getElementsByTagName('button'),
+        all = document.getElementsByClassName('collapsible-title');
 
   // expand and shrink
   expanded[0].classList.toggle('active');
@@ -14,10 +14,64 @@ function expand(elem) {
   activeBtn[0].children[0].textContent = 'keyboard_arrow_down'
 
   // set active button to disabled and rest to enabled
-  for(var i=0; i < all.length; i++) {
+  for(let i = 0; i < all.length; i++) {
     all[i].getElementsByTagName('button')[0].disabled = false;
     all[i].getElementsByTagName('button')[0].style.cursor = 'pointer';
   }
   elem.disabled = true;
   elem.style.cursor = 'default';
+}
+
+// get all elements that should be counted up
+const statistics = document.getElementById('statistics'),
+      statItems = statistics.querySelectorAll('h3');
+let isVisible = false;
+
+// event listener for scroll
+window.addEventListener('scroll', () => {
+  if(isInViewport(statistics)) {
+    if(isVisible === false) {
+      isVisible = true;
+      for(let i = 0; i < statItems.length; i++) {
+        startCounting(statItems[i], 0, Number(statItems[i].innerHTML), 3000)
+      }
+    }
+  }
+}, false);
+
+// check if statistics element is in viewport
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom - 80 <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
+// start counting
+function startCounting(el, start, end, duration) {
+  const range = end - start,
+        startTime = new Date().getTime(), 
+        endTime = startTime + duration;
+  
+  let timer,
+      stepTime = Math.abs(Math.floor(duration / range));
+
+  // count up
+  function run() {
+    const now = new Date().getTime(),
+          remaining = Math.max((endTime - now) / duration, 0),
+          value = Math.round(end - (remaining * range));
+
+    el.innerHTML = value;
+    if(value == end) {
+      clearInterval(timer);
+    }
+  }
+
+  // run count up function
+  timer = setInterval(run, stepTime);
+  run();
 }
